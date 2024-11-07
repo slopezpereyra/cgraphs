83e6c75070761a15f2cd5eae5c41f7ebc825e0ea
santi <santiagolopezpereyra@gmail.com>
2024-11-07 18:30:05 -0300

    updated greedy flow test

--- c/test_greedyflow.c
+++ c/test_greedyflow.c
@@ -8,17 +8,18 @@
 
 void test_greedyflow(){
 
-    Graph *G = readGraph("graphs/greedyflow.txt");
+    Graph *G = readGraph("graphs/network.txt");
+    printGraph(G);
     assert(G != NULL);
 
-    greedyFlow(G, 0, 5, flowBFS);
+    greedyFlow(G, 0, 9, flowBFS);
 
 
-    Graph *W = readGraph("graphs/greedyflow.txt");
+    Graph *W = readGraph("graphs/network.txt");
     assert(W != NULL);
 
     printf("Attempting FLOW DFS\n");
-    greedyFlow(W, 0, 5, flowDFS);
+    greedyFlow(W, 0, 9, flowDFS);
     printGraph(W);
     printGraph(G);
 
